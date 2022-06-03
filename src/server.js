a8ad16f10c4b109ce08381bb13b1d24517cf18e3
Henrry Hernandez <jenrruhndz@gmail.com>
2022-06-03 06:27:02 +0200

    feat: add connection to database; configure environment variables

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 class Server {
@@ -11,12 +12,27 @@ class Server {
     this.port = process.env.PORT || 8000;
 
     this.middlewares();
+    this.database();
     this.routes();
   }
 
   middlewares() {
     this.app.use(cors());
     this.app.use(express.json());
+  }
+
+  database() {
+    const DB = process.env.DATABASE.replace(
+      "<PASSWORD>",
+      process.env.DATABASE_PASSWORD
+    );
+
+    mongoose
+      .connect(DB)
+      .then((conn) => {
+        console.log("DB connection successful");
+      })
+      .catch((error) => console.log(error));
   }
 
   routes() {}
